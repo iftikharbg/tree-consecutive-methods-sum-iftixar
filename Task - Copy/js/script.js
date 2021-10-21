ccf68f813751061423f81606fb7ff0dacab29473
iftikharbg <iftikharbg@code.edu.az>
2021-10-21 13:31:23 +0400

    Homework

---
+++ Task - Copy/js/script.js
@@ -0,0 +1,94 @@
+1.// function ChristmasTree(n){
+//     for ( i = 0; i <=n; i++) {
+//         let tree = ' ';
+//         for ( j = 1; j <= n-i; j++) {
+//             tree+= " ";
+//         };
+//         for ( k = 0; k <= i; k++) {
+            
+//             tree += " *"
+//         };
+//         console.log(tree);
+        
+//     };
+// };
+
+// ChristmasTree(6);
+
+
+
+
+
+2.// function GetSequence(a,b) {
+
+//     let NewArray = []
+
+//     for (let i = a; i <= b; i++) {
+        
+//         NewArray.push(i);
+        
+//     };
+//     console.log(NewArray);
+    
+// };
+// GetSequence(1,5);
+
+
+
+
+3.//let NewArray = [32,25,85,89,45,76,7,41,2,12]
+
+//a//console.log(NewArray);
+
+//b //alert(Math.max(...NewArray));
+
+//c  //NewArray.push(15);
+  //console.log(NewArray);
+
+//d //NewArray.shift(0);
+  //console.log(NewArray);
+
+//e //NewArray.sort();
+   //NewArray.pop();
+   //console.log(NewArray);
+
+//f //NewArray.splice(3,0,43);
+   //console.log(NewArray);
+
+   
+//g //console.log(NewArray.slice(3));
+
+
+//h //NewArray.forEach(number => console.log(number));
+ 
+
+
+//i //let SecondArray = []
+
+//   for ( i = 0; i < NewArray.length; i++) {
+      
+//     if(NewArray[i] > 40){
+        
+//         SecondArray.push(NewArray[i]);
+//     };
+      
+//   };
+
+// console.log(SecondArray);
+
+
+
+
+
+4.// function Sum(a,b) {
+
+//     if(a==b){
+//         console.log((a+b)*3);
+//     }
+//     else{
+//         console.log(a+b);
+//     }
+    
+// }
+// Sum(5,5);
+
